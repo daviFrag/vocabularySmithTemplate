f0cd0dbe13d8201193ee9b8440d90867f50038c5
Davide Frageri <davide.frageri@studenti.unitn.it>
2024-01-29 16:58:45 +0100

    remove unused fields

--- src/main/java/com/vocabularies/template/readers/ExamplePayload.java
+++ src/main/java/com/vocabularies/template/readers/ExamplePayload.java
@@ -4,14 +4,10 @@ import com.rulesmith.vocabulary.states.StatePayload;
 
 public class ExamplePayload implements StatePayload{
     private String eventType;
-    private int insertedCardIndex;
-    private String[] cardArray;
     
 
     public ExamplePayload(String eventType, Integer insertedCardIndex, String[] cardArray) {
         this.eventType = eventType;
-        this.insertedCardIndex = insertedCardIndex;
-        this.cardArray = cardArray;
     }
 
     // getters
@@ -19,28 +15,8 @@ public class ExamplePayload implements StatePayload{
         return this.eventType;
     }
 
-    public Integer getInsertedCardIndex() {
-        return this.insertedCardIndex;
-    }
-
-    public String[] getCardArray() {
-        return this.cardArray;
-    }
-
-    public String getCurrentCard() {
-        return this.cardArray[this.insertedCardIndex];
-    }
-
     // setters
     public void setEventType(String eventType) {
         this.eventType = eventType;
-    }
-
-    public void setInsertedCardIndex(Integer insertedCardIndex) {
-        this.insertedCardIndex = insertedCardIndex;
-    }
-
-    public void setCardArray(String[] cardArray) {
-        this.cardArray = cardArray;
     }
 }
